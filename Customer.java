b7e28e7bfff79d35939c8e3b9e249333025d1e33
Jessa Mae Codizal <jessamcodizal@gmail.com>
2024-11-18 13:05:05 +0800

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,47 @@
+package waterrefillingsalesystem;
+
+public class Customer {
+    private final String name;
+    private final String address;
+    private final String containerType;
+    private final String paymentMethod;
+    private final String gcashNumber;
+    private final String gcashName;
+    private final int gcashAmount;
+    private final int quantity;
+
+    // Constructor
+    public Customer(String name, String address, String containerType, String paymentMethod, String gcashNumber, 
+                    String gcashName, int gcashAmount, int quantity) {
+        this.name = name;
+        this.address = address;
+        this.containerType = containerType;
+        this.paymentMethod = paymentMethod;
+        this.gcashNumber = gcashNumber;
+        this.gcashName = gcashName;
+        this.gcashAmount = gcashAmount;
+        this.quantity = quantity;
+    }
+
+    // Getter for address
+    public String getAddress() {
+        return address;
+    }
+
+    // Other getters
+    public String getContainerType() {
+        return containerType;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public int getGcashAmount() {
+        return gcashAmount;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
